fcb2d916de111a72e3f46081e5cf9201ef0994d4
Nicolas Echeverria <nicoecheok@gmail.com>
2023-03-27 05:12:55 -0300

    PreEntrega1Echeverria

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <NavBar/>
-    <ItemListContainer greeting="hola mundillo"/>
+    <ItemListContainer greeting="Bienvenidos a nuestra TIENDA"/>
     <ItemCount/>
     <Vista/>
     </>
--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -4,43 +4,19 @@ import CartWidget from '../CartWidget/CartWidget'
 
 const NavBar = () => {
   return (
-/*     <header>
-        <h1>Marolio Online</h1>
-        <nav>
-            <ul>
-                <li>Lacteos</li>
-                <li>Bebidas</li>
-                <li>Almacen</li>
-            </ul>
-        </nav>
-        <CartWidget/>
-        
-    </header> */
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <header>
+      <h1>Sol Accursio Pasteleria</h1>
+      <nav>
+        <ul>
+          <li>Tortas</li>
+          <li>Regalos</li>
+          <li>Bombones</li>
+        </ul>
       </nav>
-    );
-  }
+      <CartWidget />
+
+    </header>
+  );
+}
 
 export default NavBar

b77c425bfada7c9df41bf739666d6c01d7438525
Nicolas Echeverria <nicoecheok@gmail.com>
2023-04-10 02:12:02 -0300

    asynmock

---
+++ src/asyncmock.js
@@ -0,0 +1,14 @@
+const products = [
+    {nombre: "Medialunas", precio: 200, id: 1, img: "./img/medialunas.jpg"},
+    {nombre: "Brownie", precio: 500, id: 2, img: "./img/brownie.png"},
+    {nombre: "Cookies", precio: 100, id: 3, img: "./img/cookies.jpg"},
+    {nombre: "Macarons", precio: 300, id: 4, img: "./img/macarons.jpg"},
+]
+
+export const getProducts = () => {
+    return new Promise((resolve) => {
+        setTimeout( () => {
+            resolve(products);
+        }, 2000)
+    })
+}
--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './ItemListContainer.css'
+import { useState, useEffect } from 'react'
+import { getProducts } from '../../asyncmock'
+import ItemList from '../ItemList/ItemList'
+
 
 const ItemListContainer = (props) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts()
+      .then(response => setProducts(response))
+      .catch(error => console.error(error))
+  }, [])
+
+
   return (
     <div>
-        <h2 className='subtitulo'>{props.greeting} </h2>
+      <h2 className='subtitulo'>{props.greeting} </h2>
+      <h2>Productos</h2>
+      <ItemList products={products}/>
     </div>
   )
 }
